ff72dc2ba8d649f6498a455910546656051e165e
Isa Dzhumabaev <isa@congruity360.com>
2021-05-20 17:33:30 +0600

    upl

--- setup_bash.sh
+++ setup_bash.sh
@@ -1,6 +1,9 @@
-cp ./bash/.bash_profile ~/.bash_profile;
+mv ~/.bash_profile ~/.pre_setup_bash_profile
+cat ./bash/.bash_profile ~/.pre_setup_bash_profile > ~/.bash_profile;
+
 cp ./bash/.git-bash-for-mac.sh /Users/isa/.git-bash-for-mac.sh;
 cp ./bash/.git-completion.bash /Users/isa/.git-completion.bash;
 cp ./bash/.git-prompt.sh /Users/isa/.git-prompt.sh;
+
 source ~/.bash_profile;
 echo 'BASH setup done.'
